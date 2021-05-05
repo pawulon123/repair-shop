3dc1baf4fa1d17a2e534518c8bf6c3aca60e9a55
pawelchodzba123 <pawul0n@op.pl>
2021-05-05 22:29:21 +0200

    added localTime employ

--- src/main/java/com/wsiiz/repairshop/servicing/domain/service/Employee.java
+++ src/main/java/com/wsiiz/repairshop/servicing/domain/service/Employee.java
@@ -3,10 +3,13 @@ package com.wsiiz.repairshop.servicing.domain.service;
 import javax.persistence.Embeddable;
 import lombok.Data;
 
+import java.time.LocalTime;
+
 @Embeddable
 @Data
 public class Employee {
 
   Long employeeId;
   String employeeName;
+  LocalTime hireData;
 }
